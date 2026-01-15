4b998cad0fb7ce53f48a86caa2cef7103b230080
Alex Patterson <alex@codingcat.dev>
2026-01-15 16:10:46 -0500

    update api default

--- app/web/sanity/lib/env.ts
+++ app/web/sanity/lib/env.ts
@@ -1,6 +1,6 @@
 export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'gj7uitls'
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
-export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-01-01'
+export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2026-01-15'
 export const useCdn = process.env.NODE_ENV === 'production'
 // Server token: Keep secret, used for server-side draft content fetching
 export const serverToken = process.env.SANITY_API_READ_TOKEN || ''
